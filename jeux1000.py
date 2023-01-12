5556bbfcf35d20bebdd12f8f9b1722b642acb7ec
Jonathan Desrochers <JonathanDesrochers@hotmail.com>
2023-01-11 20:43:47 -0500

    New version
    Signed-off-by: Jonathan Desrochers <JonathanDesrochers@hotmail.com>

---
+++ jeux1000.py
@@ -0,0 +1,14 @@
+
+
+class Player:
+    def __init__(self, name, IA, position, roundPlays ):
+        self.name = name
+        self.IA = False
+        self.position = None
+        self.roundPlays = None
+        
+        
+        
+objJonathan = Player("Jonathan", False, 0,0)
+        
+        
